a5154814f9698a26f7bf291355ad952c21ee516a
sudomachine <su-d-a-k-o-v@yandex.ru>
2021-12-16 01:53:27 +0300

    Hand class was added.

--- blackjack.cpp
+++ blackjack.cpp
@@ -1,8 +1,23 @@
-
+#include <iostream>
 #include <vector>
 
-enum Suit {CLUBS = 1, DIAMONDS, HEARTS, SPADES};
-enum Dignity {1 = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+enum Suit {CLUBS = 1,
+	   DIAMONDS,
+	   HEARTS,
+	   SPADES};
+enum Dignity {TWO = 2,
+	      THREE,
+	      FOUR,
+	      FIVE,
+	      SIX,
+	      SEVEN,
+	      EIGHT,
+	      NINE,
+	      TEN,						
+	      JACK = 10,
+	      QUEEN = 10,
+	      KING = 10,
+	      ACE = 11};
 
 class Card
 {
@@ -11,7 +26,7 @@ private:
   Dignity m_dignity;
   bool m_visibillity;
 public:
-  Card(int suit, int dignity, bool visibillity):
+  Card(Suit suit, Dignity dignity, bool visibillity):
     m_suit(suit), m_dignity(dignity), m_visibillity(visibillity)
   {
     // constr
@@ -24,33 +39,80 @@ public:
   {
     m_visibillity = (m_visibillity) ? false : true;
   }
-  int getDignity(Card *card) const
+  int getDignity() const
   {
-    return card->m_dignity;
+    return this->m_dignity;
+  }
+  void print()
+  {
+    std::cout << "CARD" << std::endl;
+    std::cout << "m_suit: " << m_suit << std::endl;
+    std::cout << "m_dignity: " << m_dignity << std::endl;
+    std::cout << "m_visibillity: " << ((m_visibillity) ? "Visible":"Unvisible") << std::endl;
   }
 };
 
 class Hand
 {
 private:
-  std::vector<Card> **m_cards;
+  std::vector<Card> *m_hand;
 public:
   Hand()
   {
+    m_hand = new std::vector<Card>;
+  }
+  Hand(std::vector<Card> *hand): m_hand(hand)
+  {
     // constr
-    
   }
   ~Hand()
   {
-    //destr
+    delete[] m_hand;
   }
-  void add(const Card *card){}
-  void clear(){}
-  int getValue(){return 0;}
+  void add(Card *card)
+  {
+    m_hand->push_back(*card);
+  }
+  void clear()
+  {
+    m_hand->clear();
+  }
+  int getValue()
+  {
+    Card *card;
+    int score = 0;
+    int qAce = 0;
+    const short ALT_ACE = 1;
+    if (m_hand->size() > 0)
+      {
+        for (int i = 0; i < m_hand->size(); i++)
+	  {
+	    score += (m_hand->begin()+i)->getDignity();
+	  }
+      }
+    if (score > 21)
+      {
+	for (int i = 0; i < m_hand->size(); i++)
+	  {
+	    if ((m_hand->begin()+i)->getDignity() == ACE)
+	      {
+		qAce++;
+	      }
+	  }
+      }
+    return score - qAce*ACE + qAce*ALT_ACE;
+  }
 };
 
 int main()
-{
+{   
+  Hand *hand = new Hand();
+  std::cout << hand->getValue() << std::endl;
+  hand->add(new Card(DIAMONDS, ACE, true));
+  hand->add(new Card(DIAMONDS, THREE, true));
+  hand->add(new Card(DIAMONDS, FIVE, true));
+  hand->add(new Card(DIAMONDS, TWO, true));
+  std::cout << hand->getValue() << std::endl;
   return 0;
 }
 
